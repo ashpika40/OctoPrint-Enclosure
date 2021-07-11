25d902aa0828512d0f78d3451794604689e0e4ad
ashpika40 <60486420+ashpika40@users.noreply.github.com>
2021-07-11 15:04:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "enclosurev2"
+plugin_identifier = "enclosure"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_enclosurev2"
+plugin_package = "octoprint_enclosure"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-EnclosureV2"
+plugin_name = "OctoPrint-Enclosure"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "4.13.2"
+plugin_version = "4.13.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = "Awesome plugin to control everything that you ever wanted on your enclosure"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Harsha Jilludimudi"
+plugin_author = "Vitor Henrique"
 
 # The plugin's author's mail address.
-plugin_author_email = "ashpika40@gmail.com"
+plugin_author_email = "vitormhenrique@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/ashpika40/OctoPrint-Enclosure"
+plugin_url = "https://github.com/vitormhenrique/OctoPrint-Enclosure"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
