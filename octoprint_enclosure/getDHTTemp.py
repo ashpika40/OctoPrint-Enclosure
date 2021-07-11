9069bab697951b07bc123434e216dc7affb4d2c8
ashpika40 <60486420+ashpika40@users.noreply.github.com>
2021-07-11 17:52:41 -0600

    Update getDHTTemp.py

--- octoprint_enclosure/getDHTTemp.py
+++ octoprint_enclosure/getDHTTemp.py
@@ -5,15 +5,17 @@ import adafruit_dht
 
 
 # Parse command line parameters.
-sensor_args = { '11': DHT11,
-    '22': DHT22 }
+sensor_args = { '11': adafruit_dht.DHT11, '22': adafruit_dht.DHT22}
+pin_args= { '11' : D17, '12' : D18 }
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
+  sensor =sensor_args[sys.argv[1]]
+  pin = pin_args[sys.argv[2]]
+ # pin = sys.argv[2]
 else:
-    sys.exit(1)
+  sys.exit(1)
 
-dhtDevice = adafruit_dht.sensor(board.pin)
+dhtDevice = sensor(pin)
+
 
 #humidity, temperature = Adafruit_DHT.read_retry(sensor, pin,2,0.5)
 
