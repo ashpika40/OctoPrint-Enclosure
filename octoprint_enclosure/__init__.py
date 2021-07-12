539dde11d8eacfbf4cd6bde72153f725b74f0695
ashpika40 <60486420+ashpika40@users.noreply.github.com>
2021-07-11 19:25:08 -0600

    Update __init__.py

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -2120,7 +2120,7 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
 
 
 __plugin_name__ = "Enclosure Plugin"
-__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_pythoncompat__ = ">=3,<4"
 
 
 def __plugin_load__():
